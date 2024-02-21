6e2b76d1208befd294590e8a95dacd900a2a54c4
machadogg <gabriel.gm91@gmail.com>
2024-02-21 21:32:15 +0000

    Test using argv from terminal

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
+#include<string.h>
 
 int main(int argc,char **argv){
 	
 	printf("Hello World!\n");
+	char argument[10];
+
+	strcpy(argument,argv[1]);
+
+	printf("%s was typed.\n",argument);
+
 
 	return 0;
 
