139bdd21fecc70922f9db31a73ad0d3d7c355e77
machadogg <gabriel.gm91@gmail.com>
2024-02-22 14:08:11 +0000

    Fixed inputs

--- Restaurant_Example/main.c
+++ Restaurant_Example/main.c
@@ -8,7 +8,7 @@ int main(){
     char confirmation;
 
     option = chooseOption();
-    fgetc(stdin);
+    
     confirmChoice(option);
     fgetc(stdin);
     printf("\nWould you like to proceed? (y or n): " );
@@ -21,7 +21,7 @@ int main(){
             printf("\nPlease choose a new option: ");
             
             option = chooseOption();
-            fgetc(stdin);
+            // fgetc(stdin);
             confirmChoice(option);
             fgetc(stdin);
             printf("\n Would you like to proceed? (y or n): " );
@@ -38,11 +38,15 @@ int main(){
         }
     }
 
-    printf("\nYour meal is being prepared. Press Enter to exit...\n");
+    if (option != 5){
+        printf("\nYour meal is being prepared. Press Enter to exit...\n");
+    } else{
+        printf("\nThank you for coming, safe travels! Press Enter to exit...\n");
+    }
     
     confirmation = '\0';
     confirmation = fgetc(stdin);
-    fgetc(stdin);
+    // fgetc(stdin);
 
     return 0;
 
