47b7aff85b64507a438e2316daf876ad80f8e2f6
machadogg <gabriel.gm91@gmail.com>
2024-02-29 01:35:04 +0000

    Working, still some issues with Scanf

--- Matrix_Multiplication/functions.c
+++ Matrix_Multiplication/functions.c
@@ -1,10 +1,10 @@
 #include "functions.h"
 
-void readMatrix(int **A, int **B, int *cols1, int *cols2, int *rows1, int *rows2){
+void getDimensions(int *cols1, int *cols2, int *rows1, int *rows2){
 	
 	printf("This application multiplies 2 matrices, M = AxB.\nA is an nXm matrix and B is an mXq matrix.");
 	printf("\nPlease enter the number of rows and columns for Matrix A: ");
-	scanf("%d %d",rows1, cols1);
+	scanf("%d %d",rows1, cols1); //rows and cols are already pointers to the address, so & is not required.
 	printf("Matrix A -> A[%d][%d]", *rows1, *cols1);
 	printf("\n\nWe have automatically assigned the number of rows for B = %d", *cols1);
 	
@@ -13,67 +13,53 @@ void readMatrix(int **A, int **B, int *cols1, int *cols2, int *rows1, int *rows2
 	scanf("%d",cols2);
 	printf("Matrix B -> B[%d][%d]\n", *rows2, *cols2);
 
-	/*
-	int rows = 2;
-    int columns = 5;
-    
-    int **matrix = (int **) malloc(rows * sizeof(int *));
-    
-    for (int i = 0; i < rows; i++) {
-        matrix[i] = (int *) malloc(columns * sizeof(int));
-    }	
-	*/
+}
 
-	A = (int **)malloc(*rows1 *sizeof(int *)); // Allocate the memory for the rows in Matrix A
-	
-	// Allocate the memory for the columns in Matrix A
-	for(int i=0; i<*rows1; i++){
-		A[i] = (int *)malloc(*cols1 *sizeof(int));
+// Reads the user input to create each matrix
+void getMatrix(int **Matrix, int rows, int cols){
+
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < cols; j++){
+			scanf("%d", &Matrix[i][j]); // Here the & is required as we are passing the exact address for the
+		}								// matrix element to be stored. Matrix[i][j] is the integer variable
+	}									// and therefore &Matrix[i][j] is required to pass the address of that
+										// element
+}
+
+/* This function returns a matrix as a pointer to a pointer (one pointer for the rows and the other
+points to the columns)
+It requires the two matrices to be multiplied, also done with pointers
+As rows and columns are not going to be manipulated anymore, we just need their values
+and not pointers to their address in the memory.
+*/
+int **multiplyMatrices(int **A, int **B, int rows1, int cols1,int rows2, int cols2){
+
+	// Allocates the rows in the memory
+	int **result = (int **)malloc(rows1 * sizeof(int *));
+	// Allocates the columns for each row (that is why you do it in a for loop) in the memory.
+	for(int i = 0; i < rows1; i++){
+		result[i] = (int *)malloc(cols2*sizeof(int));
 	}
 	
-	if(A==NULL){
-		printf("\nError allocating memory for Matrix A.\n");
-		exit(1);
-	}
-
-	B = (int **)malloc(*rows2 * sizeof(int *));// Allocate the memory for the rows in Matrix B
-	
-	// Allocate the memory for the columns in Matrix B
-	for(int i=0; i<*rows2; i++){
-		B[i] = (int *)malloc(*cols2 *sizeof(int));
-	}
-
-	if(B==NULL){
-		printf("\nError allocating memory for Matrix B.\n");
-		exit(1);
-	}
-
-	printf("\nMemory successfuly allocated\n");
-    // Input elements for matrices
-	printf("\nTime to input the elements.\n");
-    printf("\nFor A[%d][%d]:\n", *rows1, *cols1);
-
-	for(int i = 0; i < *rows1; i++){
-		for(int j = 0; j < *cols1; j++){
-			scanf("%d", &A[i][j]);
-		}
-	}
-    
-	printf("\nFor B[%d][%d]:\n", *rows2, *cols2);
-
-	for(int i = 0; i < *rows2; i++){
-		for(int j = 0; j < *cols2; j++){
-			scanf("%d", &B[i][j]);
+	// Iterative algorithm to multiply both matrices
+	for(int i = 0; i<rows1; i++){
+		for(int j=0; j<cols2; j++){
+			result[i][j] = 0;
+			for(int k=0; k<cols1; k++){
+				result[i][j] += A[i][k] * B[k][j];
+			}
 		}
 	}
 
-	printf("A line 1 = [%d %d %d]",A[0][0],A[0][1],A[0][2]);
+	// Returns the result, which is a pointer to a pointer (same type as the function)
+	return result;
 
 }
 
-void printMatrix(int **Matrix, int rows, int cols){
-
-
+// Receives the matrix by reference and rows and columns by value
+void printMatrix(int **Matrix, int rows, int cols){ 
+//Iterates over the matrix element and prints the first row, then the second, and so on.
+//After each for loop iterating int j (cols), a \n is used to print the next row.
 	for(int i = 0; i< rows; i++){
 		for(int j = 0; j < cols; j++){
 			printf("%d ",Matrix[i][j]);
@@ -82,3 +68,4 @@ void printMatrix(int **Matrix, int rows, int cols){
 	}
 
 }
+
--- Matrix_Multiplication/functions.h
+++ Matrix_Multiplication/functions.h
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void readMatrix(int **A, int **B, int *cols1, int *cols2, int *rows1, int *rows2);
+void getDimensions(int *cols1, int *cols2, int *rows1, int *rows2);
+
+void getMatrix(int **Matrix, int rows, int cols);
 
 void printMatrix(int **Matrix, int rows, int cols);
+
+int **multiplyMatrices(int **A, int **B, int rows1, int cols1,int rows2, int cols2);
